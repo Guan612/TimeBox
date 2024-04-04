bfcdeddb962922884c1114c90dca01d01bb6135b
Guan612 <tiantony53@163.com>
2024-04-04 20:07:50 +0800

    Update index.js

--- Sever/src/router/index.js
+++ Sever/src/router/index.js
@@ -1,8 +1,11 @@
 const Rouret = require('@koa/router')
 const router = new Rouret()
 const userRouter = require('./user.router')
+const photoRouter = require('./photo.router')
 
 //挂载用户路由
 router.use('/users', userRouter.routes())
+router.use('/photos', photoRouter.routes())
+
 
 module.exports = router
