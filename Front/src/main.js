0ae6c43c4312984716410bf731e99985fa3411d0
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-04-07 08:18:04 +0800

    组件更新

--- Front/src/main.js
+++ Front/src/main.js
@@ -2,10 +2,21 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+
 import App from './App.vue'
 import router from './router'
 
-const app = createApp(App)
+const app = createApp(App);
+
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+};
+
+app.use(ElementPlus, {
+    locale: zhCn,
+})
 
 app.use(createPinia())
 app.use(router)
