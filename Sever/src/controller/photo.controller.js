428e0cd3b927236a3242df0da9a22e8136d53ed2
Guan612 <tiantony53@163.com>
2024-04-09 20:28:23 +0800

    搭建照片后端框架

--- Sever/src/controller/photo.controller.js
+++ Sever/src/controller/photo.controller.js
@@ -1,4 +1,5 @@
 const {getAll} = require('../service/photo.service')
+
 class photoController {
     //获取所有照片
     async getAllPhoto(ctx) {
@@ -7,6 +8,28 @@ class photoController {
         ctx.body = {
             code: 0,
             msg: '获取照片成功',
+            reslut: res
+        }
+    }
+
+    //获取照片合集详细信息
+    async getDetailedColl(ctx){
+        const {id} = ctx.request.query;
+        const res = await getDetailed(id);
+        ctx.body = {
+            code: 0,
+            msg: '获取照片详细信息成功',
+            reslut: res
+        }
+    }
+
+    //添加照片集
+    async addPhotoColl(ctx){
+        const {name,description,cover,userId} = ctx.request.body;
+        const res = await addPhotoColl({name,description,cover,userId});
+        ctx.body = {
+            code: 0,
+            msg: '添加照片集成功',
             reslut: res
         }
     }
--- Sever/src/router/photo.router.js
+++ Sever/src/router/photo.router.js
@@ -2,6 +2,19 @@ const Router = require('@koa/router');
 const router = new Router();
 const { getAllPhoto } = require('../controller/photo.controller');
 
+//获取照片列表
 router.get('/',getAllPhoto)
+
+//获取照片合集详细信息(photo_collection_id)
+router.get('/:id')
+
+//添加照片集
+router.post('/')
+
+//修改照片合集信息
+router.put('/:id')
+
+//删除照片合集
+router.delete('/:id')
 
 module.exports = router;
