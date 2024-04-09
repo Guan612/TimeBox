e99bb24160cdff9d2b90fa8e45ded4444be3496b
Guan612 <tiantony53@163.com>
2024-04-09 19:27:04 +0800

    Update index.js

--- Front/src/apis/index.js
+++ Front/src/apis/index.js
@@ -2,7 +2,7 @@ import axios from "axios"
 import {useUserStore} from '@/stores/userStore'
 
 const http =  axios.create({
-    baseURL: 'http://127.0.0.1:5000',
+    baseURL: 'http://127.0.0.1:8000',
     timeout: 2000
 })
 
