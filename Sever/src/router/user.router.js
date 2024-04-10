edc973f728518e6e4fb84bc716bf899f6c111725
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-04-10 11:17:29 +0800

    Update user.router.js

--- Sever/src/router/user.router.js
+++ Sever/src/router/user.router.js
@@ -1,18 +1,40 @@
 const Router = require('@koa/router');
 const router = new Router();
 
-const {login,register,changePwd,changeNickName} = require('../controller/user.controller')
+const {
+    login,
+    register,
+    changePwd,
+    changeNickName,
+    test
+} = require('../controller/user.controller')
+
+const {
+    userValidator,
+    userExistValidator,
+} = require('../middleware/user.middleware')
 
 //登录
-router.post('/login',login)
+router.post('/login', userValidator({
+    loginid: "string",
+    password: "string",
+}), login)
 
 //注册
-router.post('/register',register)
+router.post('/register', userValidator({
+    loginid: "string",
+    password: "string",
+    email:{type:"email",required:false},
+    nickname:{type:"string",required:false},
+    phone:{type:"string",required:false},
+}), userExistValidator,register)
 
 //更改密码
 router.put('/changepwd',changePwd)
 
 //更改昵称
 router.put('/changenickname',changeNickName)
+
+router.post('/test',test)
 
 module.exports = router;
