c5a1b82b6e63c8308118633c164a1e1941babd26
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-04-10 11:16:55 +0800

    添加错误处理

---
+++ Sever/src/app/errHandler.js
@@ -0,0 +1,17 @@
+module.exports = (err, ctx) => {
+    let status = 500;
+    switch (err.code) {
+        case '10001':
+            status = 400;
+            break;
+        case '10002':
+            status = 409;
+            break;
+        default:
+            status = 500;
+    }
+    // 设置 HTTP 状态码和响应体
+    ctx.status = status;
+    ctx.body = err;
+    console.log(err);
+}
--- Sever/src/app/index.js
+++ Sever/src/app/index.js
@@ -1,12 +1,18 @@
 const Koa = require('koa');
 const {koaBody} = require('koa-body');
 const cors = require('@koa/cors');
+const parameter = require('koa-parameter');
+
 const router = require('../router/index');
+const errHandler = require('./errHandler');
 
 const app = new Koa();
 
+app.use(parameter(app));
 app.use(koaBody());
 app.use(cors());
 app.use(router.routes()).use(router.allowedMethods());
 
+//错误处理中间件
+app.on('error', errHandler);
 module.exports = app;
