7a237b4e75c1bf1de6a55c86ef04367b272ede9a
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-04-12 17:01:59 +0800

    Update index.vue

--- Front/src/views/Layout/index.vue
+++ Front/src/views/Layout/index.vue
@@ -1,24 +1,9 @@
 <script setup>
-import sideBar from './component/sideBar.vue'
-import haderBar from './component/haderBar.vue';
+import haderBar from './component/haderBar.vue'
 </script>
 
 <template>
-    <div class="common-layout flex-col h-screen">
-        <el-container class="">
-            <el-aside class="bg-cyan-400 flex-col hidden md:block transition duration-300 ease-out md:w-1/6">
-              <sideBar></sideBar>
-            </el-aside>
-            <el-container class="w-5/6 flex">
-                <el-header class="bg-rose-400">
-                    <haderBar></haderBar>
-                </el-header>
-                <el-main class="bg-white">
-                    <router-view></router-view>
-                </el-main>
-            </el-container>
-        </el-container>
-    </div>
+    <haderBar></haderBar>
 </template>
 
 <style scoped></style>
