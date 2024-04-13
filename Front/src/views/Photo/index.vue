d6c0a1a2295cda4a134b294f0cfcd6d10845c3fa
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-04-13 17:01:13 +0800

    首页合集卡

--- Front/src/views/Photo/index.vue
+++ Front/src/views/Photo/index.vue
@@ -1,21 +1,35 @@
 <script setup>
-import {getPhotoListAPI} from '@/apis/photo'
-import {ref, onMounted} from 'vue'
+import { getPhotoListAPI } from '@/apis/photo'
+import { ref, onMounted } from 'vue'
+import collectionCard from './component/collectionCard.vue';
 
 const photoList = ref({})
 const getphoto = async () => {
     const res = await getPhotoListAPI()
-    photoList.value = res.data
+    photoList.value = res.reslut
 }
 
 onMounted(() => {
     getphoto()
+    console.log(photoList)
 })
 </script>
 
 <template>
     <div class="flex bg-white">
-        照片主页
+        <div class="flex flex-grow">
+            <div class="justify-center grid grid-cols-1 md:grid-cols-3 xl:grid-cols-4">
+                <div v-for="item in photoList" :key="item.id">
+                    <collectionCard 
+                    :collctionName="item.photoName"
+                    :imgSrc="item.photoUrl"
+                    :collctionDes="item.photoDes"
+                    ></collectionCard>
+                </div>
+            </div>
+
+        </div>
+        <div class="w-1/5">时间线</div>
     </div>
 </template>
 
