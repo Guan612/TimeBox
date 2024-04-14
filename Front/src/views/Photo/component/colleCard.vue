74b7e8b39e1b31ff362927770023cf69c3382c55
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-04-14 21:34:04 +0800

    Update colleCard.vue

--- Front/src/views/Photo/component/colleCard.vue
+++ Front/src/views/Photo/component/colleCard.vue
@@ -1,18 +1,18 @@
 <script setup>
-import {useRoute} from 'vue-router'
+import { useRoute } from 'vue-router'
 import { onMounted, ref } from 'vue'
 import { getPhotoCollectionAPI } from '@/apis/photo'
-const {params} = useRoute();
+const { params } = useRoute();
 
 const photoCollId = ref('');
 const photoColl = ref({});
 
-const getPhotoColl = async ()=>{
-    const {data:res} = await getPhotoCollectionAPI(photoCollId.value);
-    photoColl.value = res;
+const getPhotoColl = async () => {
+    const res = await getPhotoCollectionAPI(photoCollId.value);
+    photoColl.value = res.reslut;
 }
 
-onMounted(()=>{
+onMounted(() => {
     photoCollId.value = params.id;
     getPhotoColl();
 })
@@ -20,7 +20,21 @@ onMounted(()=>{
 </script>
 
 <template>
-    <div>照片详情卡</div>
+    <div class="flex flex-row justify-center items-center w-full h-full">
+        <div class="justify-center items-center w-2/3">
+            <el-carousel :interval="5000" arrow="always">
+                <el-carousel-item v-for="item in photoColl.photoslist" :key="item.id">
+                    <el-image :src="item.photoUrl" :zoom-rate="1.2" :max-scale="7" :min-scale="0.2"
+                        :preview-src-list="srcList" :initial-index="4" fit="contain" loading="lazy" />
+                </el-carousel-item>
+            </el-carousel>
+        </div>
+        <div>
+            <div>照片名字区</div>
+            <div>照片描述区</div>
+        </div>
+
+    </div>
 </template>
 
 <style scoped></style>
