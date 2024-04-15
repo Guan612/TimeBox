79b9a5c7dc4cc612e507dba5025d941f2e8d51ad
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-04-15 15:37:18 +0800

    Update photo.js

--- Front/src/apis/photo.js
+++ Front/src/apis/photo.js
@@ -1,26 +1,31 @@
 import http from "./index";
 
 //获取照片列表
-export const getPhotoListAPI = ()=>{
+export const getPhotoListAPI = () => {
     return http.get("/photos");
 }
 
 //获取照片集详情
-export const getPhotoCollectionAPI = (pCId)=>{
+export const getPhotoCollectionAPI = (pCId) => {
     return http.get(`/photos/${pCId}`);
 }
 
 //添加照片集
-export const addPhotoCollectionAPI = (data)=>{
-    return http.post("/photos",data);
+export const addPhotoCollectionAPI = (data) => {
+    return http.post("/photos", data);
 }
 
 //修改照片集
-export const updatePhotoCollectionAPI = (pCId,data)=>{
-    return http.put(`/photos/${pCId}`,data);
+export const updatePhotoCollectionAPI = (pCId, data) => {
+    return http.put(`/photos/${pCId}`, data);
 }
 
 //删除照片集
-export const deletePhotoCollectionAPI = (pCId)=>{
+export const deletePhotoCollectionAPI = (pCId) => {
     return http.delete(`/photos/${pCId}`);
+}
+
+//上传照片
+export const uploadPhotoAPI = (data) => {
+    return http.post("/photos/upload", data);
 }
