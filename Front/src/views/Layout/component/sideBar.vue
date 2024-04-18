5bb5741fce5b418f20cd8e753598685e8c894875
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-04-18 15:01:00 +0800

    Update sideBar.vue

--- Front/src/views/Layout/component/sideBar.vue
+++ Front/src/views/Layout/component/sideBar.vue
@@ -2,14 +2,15 @@
 import { ref } from 'vue';
 import router from '@/router';
 
-const logoImg = ref('https://upload.wikimedia.org/wikipedia/commons/1/1a/Dolby_logo_2019.svg')
 </script>
 
 <template>
   <div class="flex flex-col bg-transblue h-screen">
-    
-
-    <div class="h-10">3</div>
+    <div class="mt-5">
+      <div class="h-10">3</div>
+      <div class="h-10">4</div>
+      <div class="h-10">5</div>
+    </div>
   </div>
 </template>
 
