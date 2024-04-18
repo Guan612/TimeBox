4c73831e94f9c28a9f230e0cac2349d48f728fbc
Guan612 <tiantony53@163.com>
2024-04-18 11:27:02 +0800

    Update userCenter.vue

--- Front/src/views/User/userCenter.vue
+++ Front/src/views/User/userCenter.vue
@@ -1,9 +1,24 @@
 <script setup>
 
+
 </script>
 
 <template>
-    <h1>个人中心</h1>
+    <div class="flex flex-col">
+        <div class="m-2 md:m-5">
+            <ElCollapse>
+                <el-collapse-item title="账户信息" name="1">
+                    <div >改账户信息</div>
+                </el-collapse-item>
+                <el-collapse-item title="照片管理" name="2">
+                    <div>管理上传的照片集</div>
+                </el-collapse-item>
+                <el-collapse-item title="Consistency" name="3">
+                    <div>测速</div>
+                </el-collapse-item>
+            </ElCollapse>
+        </div>
+    </div>
 </template>
 
 <style scoped>
