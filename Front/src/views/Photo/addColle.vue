c893de4e0c81111402754b35e79bccc534259483
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-04-20 10:40:32 +0800

    Update addColle.vue

--- Front/src/views/Photo/addColle.vue
+++ Front/src/views/Photo/addColle.vue
@@ -1,11 +1,56 @@
 <script setup>
+import { ref } from 'vue';
+import {addPhotoCollectionAPI} from '@/apis/photo'
+const collinfo = ref({
+    photoname: '',
+    photodes: '',
+    photoUrl:''
+})
+
+const cratePhotoColl = async () => {
+    const {data: res} = await addPhotoCollectionAPI(collinfo.value)
+    console.log(res)
+}
+
 
 </script>
 
 <template>
-    <h1>添加照片合集</h1>
+    <div class="flex flex-col m-5">
+        <div>上传区域</div>
+        <ElForm v-model="collinfo" label-position="top">
+            <ElFormItem label="相册集名称">
+                <ElInput v-model="collinfo.photoname" />
+            </ElFormItem>
+            <ElFormItem label="相册集描述">
+                <ElInput v-model="collinfo.photodes" />
+            </ElFormItem>
+            <el-form-item label="相册集封面">
+                <!-- actiont填写上传api -->
+                <el-upload class="upload-demo" drag action="http://localhost:8000/photos/upload" multiple>
+                    <el-icon class="el-icon--upload"><upload-filled /></el-icon>
+                    <div class="el-upload__text">
+                        将文件拖到此处 <em>点击上传</em>
+                    </div>
+                    <template #tip>
+                        <div class="el-upload__tip">
+                            只能上传图片格式哦
+                        </div>
+                    </template>
+                </el-upload>
+            </el-form-item>
+            
+            <el-form-item>
+                <el-button type="primary" @click="cratePhotoColl">创建</el-button>
+                <el-button>取消</el-button>
+            </el-form-item>
+        </ElForm>
+        <div class="">
+
+        </div>
+        <div>相册集创建者区域</div>
+        <div>相册集是否公开区域</div>
+    </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
