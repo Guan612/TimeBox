1fa51540ca58666592514567b00c3fd2435a255b
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-04-20 16:25:55 +0800

    上传带token参数
    还是没搞定

--- Front/src/views/Photo/uplaodPhoto.vue
+++ Front/src/views/Photo/uplaodPhoto.vue
@@ -1,13 +1,20 @@
 <script setup>
 import { ElFormItem } from 'element-plus';
 import { ref } from 'vue';
+import { useUserStore } from '@/stores/userStore';
+
+const userStore = useUserStore();
+const token = userStore.userInfo.token;
+const headers = {
+    Authorization: `Bearer ${token}`
+};
 
 </script>
 
 <template>
     <div class="flex flex-col m-5">
         <!-- actiont填写上传api -->
-        <el-upload class="upload-demo" drag action="http://localhost:8000/photos/upload" multiple>
+        <el-upload class="" drag action="http://localhost:8000/photos/upload" multiple :haders="headers">
             <el-icon class="el-icon--upload"><upload-filled /></el-icon>
             <div class="el-upload__text">
                 将文件拖到此处 <em>点击上传</em>
