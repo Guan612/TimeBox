90c21c6f0708c4ff82f4c70f09c6bf8b9082cc7d
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-04-25 09:40:51 +0800

    增加了从现有照片选择
    好抽象的上传，我喜欢

--- Front/src/views/Photo/addColle.vue
+++ Front/src/views/Photo/addColle.vue
@@ -1,6 +1,7 @@
 <script setup>
-import { ref } from 'vue';
-import { addPhotoCollectionAPI } from '@/apis/photo'
+import { ref, onMounted } from 'vue';
+import { addPhotoCollectionAPI, findMyPhotoAPI } from '@/apis/photo'
+
 const collinfo = ref({
     photoname: '',
     photodes: '',
@@ -11,6 +12,17 @@ const cratePhotoColl = async () => {
     const res = await addPhotoCollectionAPI(collinfo.value)
     console.log(res)
 }
+
+const options = ref([])
+const getMyPhoto = async () => {
+    const res = await findMyPhotoAPI()
+    //console.log(res)
+    options.value = res.reslut
+}
+
+onMounted(() => {
+    getMyPhoto()
+})
 
 
 </script>
@@ -26,20 +38,36 @@ const cratePhotoColl = async () => {
                 <ElInput v-model="collinfo.photodes" type="textarea" autosize maxlength="200" show-word-limit :rows="2"
                     clearable />
             </ElFormItem>
-            <el-form-item label="相册集封面">
-                <!-- actiont填写上传api -->
-                <el-upload class="upload-demo" drag action="http://localhost:8000/photos/upload" multiple>
-                    <el-icon class="el-icon--upload"><upload-filled /></el-icon>
-                    <div class="el-upload__text">
-                        将文件拖到此处 <em>点击上传</em>
-                    </div>
-                    <template #tip>
-                        <div class="el-upload__tip">
-                            只能上传图片格式哦
-                        </div>
-                    </template>
-                </el-upload>
+
+            <el-form-item label="照片">
+                <div class="flex flex-row">
+                    <el-form-item label="相册集封面" class="m-2 basis-1/2">
+                        <!-- actiont填写上传api -->
+                        <el-upload class="upload-demo" drag action="http://localhost:8000/photos/upload" multiple>
+                            <el-icon class="el-icon--upload"><upload-filled /></el-icon>
+                            <div class="el-upload__text">
+                                将文件拖到此处 <em>点击上传</em>
+                            </div>
+                            <template #tip>
+                                <div class="el-upload__tip">
+                                    只能上传图片格式哦
+                                </div>
+                            </template>
+                        </el-upload>
+                    </el-form-item>
+
+                    <el-form-item label="从已经上传的照片选择" class="m-2 basis-1/2">
+                        <el-select v-model="collinfo.photoUrl" placeholder="请选择">
+                            <el-option v-for="item in options" :key="item.id" :label="item.label" :value="item.value">
+                                <el-image :src="item.photoUrl"></el-image>
+                            </el-option>
+                        </el-select>
+                        <el-button type="primary" @click="">选择</el-button>
+                    </el-form-item>
+                </div>
             </el-form-item>
+
+
 
             <el-form-item>
                 <el-button type="primary" @click="cratePhotoColl">创建</el-button>
