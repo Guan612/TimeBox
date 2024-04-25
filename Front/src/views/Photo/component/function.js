545dfeefc1002c9dbe232bb1e43aa7eb9463693d
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-04-25 16:20:46 +0800

    Update function.js

--- Front/src/views/Photo/component/function.js
+++ Front/src/views/Photo/component/function.js
@@ -3,3 +3,10 @@ export const backindex = () => {
     //使用back方法返回上一级
     router.back();
 }
+
+export const toupdatecoll = (id) => {
+    //使用push方法跳转到指定页面
+    router.push({
+        path: `/updatecoll/${id}`
+    });
+}
