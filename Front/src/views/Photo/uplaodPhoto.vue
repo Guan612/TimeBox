c192ffd341ce88c2fb435e0d433bc71cc9762d85
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-05-06 10:14:46 +0800

    继续调试上传照片api

--- Front/src/views/Photo/uplaodPhoto.vue
+++ Front/src/views/Photo/uplaodPhoto.vue
@@ -3,58 +3,51 @@ import { ref } from 'vue'
 import { Plus } from '@element-plus/icons-vue'
 import { uploadPhotoAPI } from '@/apis/photo'
 
-const uploadUrl = ref('');
 const fileList = ref([]);
 const dialogVisible = ref(false);
 const dialogImageUrl = ref('');
-const uploadSuccessMessage = ref('');
 
-const handleBeforeUpload = async (file) => {
-    // 使用uploadPhotoAPI发送上传请求
-    const formData = new FormData();
-    formData.append('file', file);
+const handleRemove = (uploadFile, uploadFiles) => {
+  console.log(uploadFile, uploadFiles)
+}
 
-    try {
-        const response = await uploadPhotoAPI(formData);
-        console.log('上传成功:', response);
-    } catch (error) {
-        console.log('上传失败:', error);
-    }
-
-    // 返回false以阻止自动上传
-    return false;
-};
-
-const handleSuccess = (response, file, fileList) => {
-    fileList.value = fileList;
+const handlePictureCardPreview = (uploadFile) => {
+    dialogImageUrl.value = uploadFile.url;
     dialogVisible.value = true;
-    dialogImageUrl.value = file.url;
-    uploadSuccessMessage.value = '文件上传成功';
-};
+}
 
-const handleError = (error, file, fileList) => {
-    console.log('上传失败:', error);
-};
+const submit = async () => {
+    console.log(fileList.value)
+    const data = new FormData();
+    fileList.value.forEach(file => {
+        data.append('files', file.raw);
+    });
+    // 添加调试信息
+    console.log("FormData对象中的文件数量:", data.getAll('files').length);
+    
+    const res = await uploadPhotoAPI(data);
+    console.log(res)
+}
 
 </script>
 
 <template>
     <div class="flex flex-col m-2">
         <div>
-            <el-upload action="" :before-upload="handleBeforeUpload" :on-success="handleSuccess" :on-error="handleError"
-                :file-list="fileList" :auto-upload="false" :show-file-list="true" list-type="picture-card"
-                accept="image/*">
+            <el-upload v-model:file-list="fileList"
+                :auto-upload="false" list-type="picture-card"
+                :on-preview="handlePictureCardPreview" :on-remove="handleRemove" multiple>
                 <el-icon>
                     <Plus />
                 </el-icon>
             </el-upload>
-            <el-dialog :visible.sync="dialogVisible">
-                <img width="100%" :src="dialogImageUrl" alt />
-                <p>{{ uploadSuccessMessage }}</p>
+
+            <el-dialog v-model="dialogVisible">
+                <img w-full :src="dialogImageUrl" alt="Preview Image" />
             </el-dialog>
         </div>
         <div class="m-2">
-            <el-button type="primary">上传到服务器</el-button>
+            <el-button type="primary" @click="submit">上传</el-button>
         </div>
     </div>
 </template>
