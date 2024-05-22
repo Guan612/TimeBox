50923120e0be8d61f23ac6a95d26eec64a390fa5
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-05-22 08:31:13 +0800

    更新点击到的地方高亮

--- Front/src/views/Layout/component/sideBar.vue
+++ Front/src/views/Layout/component/sideBar.vue
@@ -1,6 +1,14 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, watch, onMounted } from 'vue';
 import router from '@/router';
+
+// 创建一个响应式变量来保存当前活动的路由
+const activeRoute = ref('/');
+
+// 点击时更新活动路由
+const updateActiveRoute = (route) => {
+  activeRoute.value = route;
+};
 
 </script>
 
@@ -8,19 +16,24 @@ import router from '@/router';
   <div class="flex flex-col bg-transblue h-screen justify-between">
     <div class="mt-5">
       <div class="m-5 p-3 bg-transblue rounded-xl hover:bg-transpink transition-all duration-300 cursor-pointer"
-        @click="router.push('/')">首页</div>
+        :class="{ 'bg-transpink': activeRoute === '/', 'bg-transblue': activeRoute !== '/' }"
+        @click="updateActiveRoute('/'); router.push('/')">首页</div>
       <div class="m-5 p-3 bg-transblue rounded-xl hover:bg-transpink transition-all duration-300 cursor-pointer"
-        @click="router.push('/myphotocoll')">我的照片集
+        :class="{ 'bg-transpink': activeRoute === '/myphotocoll', 'bg-transblue': activeRoute !== '/myphotocoll' }"
+        @click="updateActiveRoute('/myphotocoll'); router.push('/myphotocoll')">我的照片集
       </div>
       <div class="m-5 p-3 bg-transblue rounded-xl hover:bg-transpink transition-all duration-300 cursor-pointer"
-        @click="router.push('/myphoto')">我的照片</div>
-      <div class="m-5 p-3 bg-transblue rounded-xl hover:bg-transpink transition-all duration-300 cursor-pointer">
+        :class="{ 'bg-transpink': activeRoute === '/myphoto', 'bg-transblue': activeRoute !== '/myphoto' }"
+        @click="updateActiveRoute('/myphoto'); router.push('/myphoto')">我的照片</div>
+      <div class="m-5 p-3 bg-transblue rounded-xl hover:bg-transpink transition-all duration-300 cursor-pointer"
+        :class="{ 'bg-transpink': activeRoute === '/favorite', 'bg-transblue': activeRoute !== '/favorite' }">
         最爱精选
       </div>
     </div>
     <div class="self-end mt-auto">
       <div class="m-5 p-3 bg-transblue rounded-xl hover:bg-transpink transition-all duration-300 cursor-pointer"
-        @click="router.push('/about')">关于</div>
+        :class="{ 'bg-transpink': activeRoute === '/about', 'bg-transblue': activeRoute !== '/about' }"
+        @click="updateActiveRoute('/about'); router.push('/about')">关于</div>
     </div>
   </div>
 </template>
